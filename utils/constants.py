7c3f291c5b1a300486b4f692c4fa4280aaaa6efe
mcadaval <matias.cadaval@gmail.com>
2020-12-19 11:48:21 -0300

    Improve dataset generator

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,18 @@
-
 class VideoConstants:
     video_1_from_1_to_3 = "video_1_from_1_to_3.mp4"
     video_1_from_8_to_12 = "video_1_from_8_to_12.mp4"
+
+
+# Colors RGB
+RGB_RED = (255, 0, 0)
+RGB_BLUE = (0, 0, 255)
+RGB_GREEN = (0, 255, 0)
+
+# Colors BGR
+BGR_WHITE = (255, 255, 255)
+BGR_RED = (0, 0, 255)
+
+# Key ASCII codes (dec)
+DELETE_KEY_CODE = 8
+RETURN_KEY_CODE = 13
+ESC_KEY_CODE = 27
