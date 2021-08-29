573a418716b7d673d515ad36017344bfa4c15086
mcadaval <matias.cadaval@gmail.com>
2021-08-29 18:06:39 -0300

    Revert "Refactor Step"
    This reverts commit 2a3f03730de64d2180ae6b36e876b09d37a9c426.

--- player_detector/step.py
+++ player_detector/step.py
@@ -5,40 +5,31 @@ from utils.utils import ScreenManager
 
 class Step:
     def __init__(self, name: str, function, params={}, debug: bool = False, modify_original_frame=True):
-        self.screen_manager = ScreenManager.get_manager()
-        self.modify_original_frame = modify_original_frame
-        self.step = ASDStep(name, function, params, debug)
-
-    def apply(self, number, original_frame, params=None):
-        frame = original_frame
-
-        if not self.modify_original_frame:
-            frame = original_frame.copy()
-
-        frame = self.step.apply(number, frame, params)
-        self.screen_manager.show_frame(frame, self.step.name) if self.step.debug else None
-
-        return frame
-
-
-class ASDStep:
-    def __init__(self, name: str, function, params={}, debug: bool = False):
         self.log = Logger(self, LoggingPackage.player_detector)
+        self.screen_manager = ScreenManager.get_manager()
         self.name = name
         self.function = function
         self.debug = debug
         self.params = params
+        self.modify_original_frame = modify_original_frame
 
-    def apply(self, number, frame, params=None):
+    def apply(self, number, original_frame, params=None):
         if params is None:
             params = self.params
         else:
             # merge
             params = {**params, **self.params}
 
+        frame = original_frame
+
+        if not self.modify_original_frame:
+            frame = original_frame.copy()
+
         Timer.start('step {}'.format(number))
-        step_result = self.function(frame, params)
+        frame = self.function(frame, params)
         elapsed_time = Timer.stop('step {}'.format(number))
+
+        self.screen_manager.show_frame(frame, self.name) if self.debug else None
 
         self.log.log('applying', {
             "number": number,
@@ -47,4 +38,7 @@ class ASDStep:
             "cost": elapsed_time
         }) if self.debug else None
 
-        return step_result
+        if self.modify_original_frame:
+            return frame
+        else:
+            return original_frame
