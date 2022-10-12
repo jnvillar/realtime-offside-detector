ebb3d912626d15b90543b58c5d4d0290099408c8
Juan Ignacio Noli Villar <juaninolivillar@gmail.com>
2022-10-12 09:28:47 -0300

    4 dataset

--- utils/constants.py
+++ utils/constants.py
@@ -2,7 +2,7 @@ class VideoConstants:
     # andan bien: 4, 13, 15
     video_1_from_1_to_3 = "video_1_from_1_to_3.mp4"
     video_1_from_8_to_12 = "video_1_from_8_to_12.mp4"
-    video_Liverpool_Benfica_119_126 = "4_Liverpool-Benfica_119_126.mp4"
+    video_4_Liverpool_Benfica_119_126 = "4_Liverpool-Benfica_119_126.mp4"
     video_11_Estudiantes_Patronato_11_22 = "11_Estudiantes-Patronato_12_22.mp4"
     video_12_ManchesterCity_Sevilla_66_74 = "12_ManchesterCity-Sevilla_66_74.mp4"
     video_9__Udinese_Cagliari_35_41 = "9_Udinese-Cagliari_35_41.mp4"
