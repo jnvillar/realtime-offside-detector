b42cc059d7f24510c6b923652d8c15d96d9b60b0
mcadaval <matias.cadaval@gmail.com>
2022-10-22 16:01:57 -0300

    More constants video names

--- utils/constants.py
+++ utils/constants.py
@@ -2,12 +2,23 @@ class VideoConstants:
     # andan bien: 4, 13, 15
     video_1_from_1_to_3 = "video_1_from_1_to_3.mp4"
     video_1_from_8_to_12 = "video_1_from_8_to_12.mp4"
+    video_1_Arsenal_Chelsea_107_122 = "1_Arsenal-Chelsea_107_122.mp4"
     video_4_Liverpool_Benfica_119_126 = "4_Liverpool-Benfica_119_126.mp4"
-    video_11_Estudiantes_Patronato_11_22 = "11_Estudiantes-Patronato_12_22.mp4"
-    video_12_ManchesterCity_Sevilla_66_74 = "12_ManchesterCity-Sevilla_66_74.mp4"
-    video_9__Udinese_Cagliari_35_41 = "9_Udinese-Cagliari_35_41.mp4"
+    video_5_ManchesterCity_AtleticoMadrid_60_81 = "5_ManchesterCity-AtleticoMadrid_60_81.mp4"
+    video_5_ManchesterCity_AtleticoMadrid_278_292 = "5_ManchesterCity-AtleticoMadrid_278_292.mp4"
     video_6_Napoli_Fiorentina_91_98 = "6_Napoli-Fiorentina_91_98.mp4"
+    video_7_Psg_Angers_103_110 = "7_Psg-Angers_103_110.mp4"
+    video_7_Psg_Angers_156_167 = "7_Psg-Angers_156_167.mp4"
+    video_9_Udinese_Cagliari_35_41 = "9_Udinese-Cagliari_35_41.mp4"
+    video_11_Estudiantes_Patronato_11_22 = "11_Estudiantes-Patronato_12_22.mp4"
+    video_11_Estudiantes_Patronato_380_392 = "11_Estudiantes-Patronato_380_392.mp4"
+    video_12_ManchesterCity_Sevilla_66_74 = "12_ManchesterCity-Sevilla_66_74.mp4"
+    video_13_Chelsea_Milan_38_44 = "13_Chelsea-Milan_38_44.mp4"
     video_15_Valencia_Getafe_38_52 = "15_Valencia-Getafe_38_52.mp4"
+    video_17_Celta_RealMadrid_112_122 = "17_Celta-RealMadrid_112_122.mp4"
+    video_19_RealMadrid_Shakhtar_245_253 = "19_RealMadrid-Shakhtar_245_253.mp4"
+    video_19_RealMadrid_Shakhtar_20_29 = "19_RealMadrid-Shakhtar_20_29.mp4"
+    video_20_BayernMunich_ViktoriaPlzen_515_524 = "20_BayernMunich-ViktoriaPlzen_515_524.mp4"
 
 
 # Colors RGB
