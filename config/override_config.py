f60c86b7934e562c8d71efdc084fc6607b7f06ed
Juan Ignacio Noli Villar <juaninolivillar@gmail.com>
2022-11-05 15:46:42 -0300

    filter videos

--- config/override_config.py
+++ config/override_config.py
@@ -86,64 +86,6 @@ override_config = {
             }
         },
     },
-    '9_Udinese-Cagliari_35_41': {
-        'player_detector': {
-            # background_subtraction, edges, adhoc, by_color, kmeans, posta=otsu
-            'method': 'otsu',
-            'detect_every_amount_of_frames': 1,
-            'kmeans': {
-                'debug': False,
-                'klusters': 20,
-                'color_percentage': (1 / 100),  # 5%
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'ignore_contours_smaller_than': 0.06,
-                'ignore_contours_bigger_than': 1,
-            },
-            'otsu': {
-                'debug': False,
-                'ignore_contours_smaller_than': 0.06,
-                'ignore_contours_bigger_than': 1,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                # 'filter_contour_inside_other': True
-            },
-            'tophat': {
-                'debug': True
-            },
-            'by_color': {
-                'debug': False,
-                'ignore_contours_smaller_than': 0.05,
-                'ignore_contours_bigger_than': 0.5,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'filter_contour_inside_other': True
-            },
-            'adhoc': {
-                'debug': False,
-                'threshold1': 50,
-                'threshold2': 70,
-                'ignore_contours_smaller_than': 0.1,
-                'ignore_contours_bigger_than': 0.5,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'filter_contour_inside_other': True
-            },
-            'background_subtraction': {
-                'debug': False,
-                'history': 1,
-                'detect_shadows': False,
-                'var_threshold': 50,
-                'ignore_contours_smaller_than': 0.02,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller
-            },
-            'edges': {
-                'debug': True,
-                'threshold1': 50,
-                'threshold2': 70,
-                'ignore_contours_smaller_than': 0.04,
-                'ignore_contours_bigger_than': 0.5,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'filter_contour_inside_other': True
-            }
-        },
-    },
     '12_ManchesterCity-Sevilla_415_425': {
         'player_sorter': {
             # bsas, automatic_by_color, by_color, kmeans
@@ -168,62 +110,6 @@ override_config = {
             'automatic_by_color': {
                 white.color_name: team_two,
                 red.color_name: team_two
-            }
-        },
-        'player_detector': {
-            # background_subtraction, edges, adhoc, by_color, kmeans, posta=otsu
-            'method': 'kmeans',
-            'detect_every_amount_of_frames': 1,
-            'kmeans': {
-                'debug': False,
-                'klusters': 15,
-                'color_percentage': (1 / 100),  # 5%
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'ignore_contours_smaller_than': 0.045,
-                'ignore_contours_bigger_than': 1,
-            },
-            'otsu': {
-                'debug': False,
-                'ignore_contours_smaller_than': 0.02,
-                'ignore_contours_bigger_than': 1,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                # 'filter_contour_inside_other': True
-            },
-            'tophat': {
-                'debug': True
-            },
-            'by_color': {
-                'debug': False,
-                'ignore_contours_smaller_than': 0.05,
-                'ignore_contours_bigger_than': 0.5,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'filter_contour_inside_other': True
-            },
-            'adhoc': {
-                'debug': False,
-                'threshold1': 50,
-                'threshold2': 70,
-                'ignore_contours_smaller_than': 0.1,
-                'ignore_contours_bigger_than': 0.5,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'filter_contour_inside_other': True
-            },
-            'background_subtraction': {
-                'debug': False,
-                'history': 1,
-                'detect_shadows': False,
-                'var_threshold': 50,
-                'ignore_contours_smaller_than': 0.02,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller
-            },
-            'edges': {
-                'debug': True,
-                'threshold1': 50,
-                'threshold2': 70,
-                'ignore_contours_smaller_than': 0.04,
-                'ignore_contours_bigger_than': 0.5,
-                'keep_contours_by_aspect_ratio': AspectRatio.taller,
-                'filter_contour_inside_other': True
             }
         },
     },
--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def save_comparison_results(video_name, results):
 
 
 if __name__ == '__main__':
-    video_name = constants.VideoConstants.video_16_Barcelona_Valladolid_43_50
+    video_name = constants.VideoConstants.video_4_Liverpool_Benfica_422_432
 
     video_path = './test/videos' + '/' + video_name
     dataset_path = './datasets' + '/' + video_name.split(".")[0] + ".json"
