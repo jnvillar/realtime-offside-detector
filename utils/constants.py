f14b0e95890bf28c979a3e076558a139b2c9ba57
mcadaval <matias.cadaval@gmail.com>
2022-11-15 17:43:19 -0300

    Add missing video constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,10 @@
-import inspect
 
 
 class VideoConstants:
-    # andan bien: 4, 13, 15
     video_1_Arsenal_Chelsea_107_122 = "1_Arsenal-Chelsea_107_122.mp4"
     video_2_Boca_Lanus_202_216 = "2_Boca-Lanus_202_216.mp4"
+    video_3_Inter_Roma_55_67 = "3_Inter-Roma_55_67.mp4"
+    video_3_Inter_Roma_147_158 = "3_Inter-Roma_147_158.mp4"
     video_4_Liverpool_Benfica_119_126 = "4_Liverpool-Benfica_119_126.mp4"
     video_4_Liverpool_Benfica_422_432 = "4_Liverpool-Benfica_422_432.mp4"
     video_5_ManchesterCity_AtleticoMadrid_60_81 = "5_ManchesterCity-AtleticoMadrid_60_81.mp4"
@@ -16,6 +16,8 @@ class VideoConstants:
     video_8_River_ArgentinosJrs_144_154 = "8_River-ArgentinosJrs_144_154.mp4"
     video_9_Udinese_Cagliari_35_41 = "9_Udinese-Cagliari_35_41.mp4"
     video_10_Italia_Alemania_78_94 = "10_Italia-Alemania_78_94.mp4"
+    video_10_Italia_Alemania_162_173 = "10_Italia-Alemania_162_173.mp4"
+    video_10_Italia_Alemania_548_555 = "10_Italia-Alemania_548_555.mp4"
     video_11_Estudiantes_Patronato_11_22 = "11_Estudiantes-Patronato_12_22.mp4"
     video_11_Estudiantes_Patronato_380_392 = "11_Estudiantes-Patronato_380_392.mp4"
     video_12_ManchesterCity_Sevilla_66_74 = "12_ManchesterCity-Sevilla_66_74.mp4"
@@ -26,8 +28,8 @@ class VideoConstants:
     video_16_Barcelona_Valladolid_43_50 = "16_Barcelona-Valladolid_43_50.mp4"
     video_17_Celta_RealMadrid_112_122 = "17_Celta-RealMadrid_112_122.mp4"
     video_18_Sevilla_Valladolid_29_38 = "18_Sevilla-Valladolid_29_38.mp4"
-    video_19_RealMadrid_Shakhtar_245_253 = "19_RealMadrid-Shakhtar_245_253.mp4"
     video_19_RealMadrid_Shakhtar_20_29 = "19_RealMadrid-Shakhtar_20_29.mp4"
+    video_19_RealMadrid_Shakhtar_245_253 = "19_RealMadrid-Shakhtar_245_253.mp4"
     video_20_BayernMunich_ViktoriaPlzen_515_524 = "20_BayernMunich-ViktoriaPlzen_515_524.mp4"
     video_21_Roma_Ludogrets_503_510 = "21_Roma-Ludogrets_503_510.mp4"
     video_22_ManchesterCity_Brighton_539_547 = "22_ManchesterCity-Brighton_539_547.mp4"
