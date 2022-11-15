8b39bb4110063137f2aa50c1ecbf395ffd1e2a83
Juan Ignacio Noli Villar <juaninolivillar@gmail.com>
2022-11-15 16:48:18 -0300

    refactor

--- build_plots.py
+++ build_plots.py
@@ -42,27 +42,27 @@ if __name__ == '__main__':
             results_file_path = './experiments/' + video_name_without_extesion + "-" + sub_problem_suffix + ".json"
             results = get_results_json(results_file_path)
 
-            if "frame_results" in results:
-                frame_results = results["frame_results"]
+            if not "frame_results" in results:
+                continue
 
-                if sub_problem_suffix == 'intertia' and len(results["frame_results"]) > 0:
-                    frame_results = results["frame_results"][0]
+            frame_results = results["frame_results"]
 
-                    data = {
-                        'k': frame_results['k'],
-                        'inertias': frame_results['inertias'],
-                    }
+            if sub_problem_suffix == 'intertia' and len(results["frame_results"]) > 0:
+                frame_results = results["frame_results"][0]
 
-                    df = pd.DataFrame(data)
+                df = pd.DataFrame({
+                    'k': frame_results['k'],
+                    'inertias': frame_results['inertias'],
+                })
 
-                    fig.add_trace(
-                        go.Scatter(x=df["k"], y=df["inertias"], name=video_name_without_extesion, mode="lines")
-                    )
-                else:
-                    metric_values = [frame_data[metric_name] for frame_data in frame_results]
-                    fig.add_trace(
-                        go.Box(x=metric_values, name=video_name_without_extesion, boxpoints="all", hoverinfo="x",
-                               boxmean=True)
-                    )
+                fig.add_trace(
+                    go.Scatter(x=df["k"], y=df["inertias"], name=video_name_without_extesion, mode="lines")
+                )
+            else:
+                metric_values = [frame_data[metric_name] for frame_data in frame_results]
+                fig.add_trace(
+                    go.Box(x=metric_values, name=video_name_without_extesion, boxpoints="all", hoverinfo="x",
+                           boxmean=True)
+                )
 
         fig.show()
